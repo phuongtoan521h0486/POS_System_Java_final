f13ee017f20d43c4b9ec344e1d811f5138a494a9
phuongtoan521h0486 <521h0486@student.tdtu.edu.vn>
2023-11-16 21:56:40 +0700

    "First upload project"

---
+++ src/main/java/com/thd/pos_system_java_final/PosSystemJavaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.thd.pos_system_java_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSystemJavaFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosSystemJavaFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thd/pos_system_java_final/PosSystemJavaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thd.pos_system_java_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemJavaFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
