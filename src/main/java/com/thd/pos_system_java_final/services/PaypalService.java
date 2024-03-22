3d0a0a0b7171f70d8333eef2cc717a0d5de19485
phuongtoan521h0486 <521h0486@student.tdtu.edu.vn>
2024-03-23 00:56:23 +0700

    Add Paypal payment to website

---
+++ src/main/java/com/thd/pos_system_java_final/services/PaypalService.java
@@ -0,0 +1,54 @@
+package com.thd.pos_system_java_final.services;
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PaypalService {
+    @Autowired
+    private APIContext apiContext;
+
+    public Payment createPayment(Double total) throws PayPalRESTException {
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
+        amount.setTotal(String.format("%.2f", total));
+
+        Transaction transaction = new Transaction();
+        transaction.setDescription("Checkout for order");
+        transaction.setAmount(amount);
+
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+
+        Payment payment = new Payment();
+        payment.setIntent("sale");
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl("http://localhost:8888/cart/step-2");
+        redirectUrls.setReturnUrl("http://localhost:8888/cart/complete");
+        payment.setRedirectUrls(redirectUrls);
+
+        return payment.create(apiContext);
+    }
+
+    public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException{
+        Payment payment = new Payment();
+        payment.setId(paymentId);
+        PaymentExecution paymentExecute = new PaymentExecution();
+        paymentExecute.setPayerId(payerId);
+        return payment.execute(apiContext, paymentExecute);
+    }
+}
