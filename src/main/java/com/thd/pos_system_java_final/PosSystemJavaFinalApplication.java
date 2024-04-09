f538b99e11e16c93a8470430ffcf34f3bef2db97
phuongtoan521h0486 <521h0486@student.tdtu.edu.vn>
2024-04-09 20:16:20 +0700

    Fix bug UI

--- src/main/java/com/thd/pos_system_java_final/PosSystemJavaFinalApplication.java
+++ src/main/java/com/thd/pos_system_java_final/PosSystemJavaFinalApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PosSystemJavaFinalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PosSystemJavaFinalApplication.class, args);
+        System.out.println("Application started. You can access the website at http://localhost:8888");
+        System.out.println("Admin account: Username - admin, Password - 123456");
     }
 
 }
